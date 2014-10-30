05530d64b8305fd3169930bd52bd8579af746455
nphamily <nganpham@Ngans-MacBook-Air.local>
2014-10-30 15:03:47 -0700

    ageist

---
+++ ruby_script.rb
@@ -0,0 +1,25 @@
+age = rand(110)
+
+#age = 48
+
+puts 'Age is ' + age.to_s
+
+if age <= 1
+  range = 'baby'
+elsif age < 10
+  range = 'child'
+elsif age <= 12
+  range = 'tween'
+elsif age <= 19
+  range = 'teenager'
+elsif age <=40
+  range = 'adult'
+elsif age <= 65
+  range = 'middle age'
+elsif age <=100
+  range = 'senior'
+else age >100
+  range = 'record breaking'
+end
+
+puts 'The age range is a ' + range + '.'
